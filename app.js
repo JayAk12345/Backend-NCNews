cbb30734625deaddafcce6d4daf3643ed555130e
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-02 15:07:45 +0000

    adds error handling for incorrect url

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { handleBadUrls } = require("./controllers/errorController");
+const apiRouter = require("./routers/apiRouter");
 
 const app = express();
 app.use(express.json());
+
+app.get("/api", apiRouter);
+
+app.use("*", handleBadUrls);
 
 module.exports = app;
