f7cdacaba0ad9edbf141150574db8c94d6ca9ce8
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-02 15:03:15 +0000

    adds test for an incorrect url

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,21 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const { request } = require("../app.js");
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const { seed } = require("../db/seeds/seed.js");
 
 beforeEach(() => seed(testData));
+console.log(typeof seed);
 afterAll(() => db.end());
+
+describe("api/wrongpath", () => {
+  describe("GET", () => {
+    it("status:404, returns error message when incorrect URL is entered", () => {
+      return request(app)
+        .get("api/wrontpath")
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("Not found");
+        });
+    });
+  });
+});
