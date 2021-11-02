fb69a3de705761bdf874b80f642404a150747a7d
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-02 16:14:54 +0000

    adds new unit test for get request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,20 +1,34 @@
-const { request } = require("../app.js");
+const request = require("supertest");
 const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
-const { seed } = require("../db/seeds/seed.js");
+const seed = require("../db/seeds/seed.js");
+const app = require("../app.js");
 
 beforeEach(() => seed(testData));
-console.log(typeof seed);
 afterAll(() => db.end());
 
-describe("api/wrongpath", () => {
+describe("/api/wrongpath", () => {
   describe("GET", () => {
     it("status:404, returns error message when incorrect URL is entered", () => {
       return request(app)
-        .get("api/wrontpath")
+        .get("/api/wrongpath")
         .expect(404)
         .then((res) => {
           expect(res.body.msg).toBe("Not found");
+        });
+    });
+  });
+});
+
+describe.only("/api/topics", () => {
+  describe("GET", () => {
+    it("returns an object", () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then((res) => {
+          expect(typeof res.body.topics).toBe("object");
+          expect(res.body.topics.length).toBe(undefined);
         });
     });
   });
