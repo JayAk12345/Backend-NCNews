4995eaae647dac2207059a27630e167635f3c9c6
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-02 15:03:49 +0000

    adds complete seed function

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -6,27 +6,27 @@ const seed = (data) => {
   // 1. create tables
   // 2. insert data
   return db
-    .query("DROP TABLE IF EXISTS topic;")
+    .query("DROP TABLE IF EXISTS comments")
     .then(() => {
-      return db.query("DROP TABLE IF EXISTS user;");
+      return db.query("DROP TABLE IF EXISTS articles;");
     })
     .then(() => {
-      return db.query("DROP TABLE IF EXISTS article;");
+      return db.query("DROP TABLE IF EXISTS users;");
     })
     .then(() => {
-      return db.query("DROP TABLE IF EXISTS comment");
+      return db.query("DROP TABLE IF EXISTS topics;");
     })
     .then(() => {
       return db.query(
-        `CREATE TABLE topic (
-          slug VARCCHAR PRIMARY KEY NOT NULL, 
+        `CREATE TABLE topics (
+          slug VARCHAR PRIMARY KEY NOT NULL, 
           description VARCHAR NOT NULL
           );`
       );
     })
     .then(() => {
       return db.query(
-        `CREATE TABLE user (
+        `CREATE TABLE users (
           username VARCHAR PRIMARY KEY NOT NULL, 
           avatar_url VARCHAR, 
           name VARCHAR);`
@@ -34,22 +34,22 @@ const seed = (data) => {
     })
     .then(() => {
       return db.query(
-        `CREATE TABLE article (
-          article_id SERIAL PRIMARY KEY, 
+        `CREATE TABLE articles (
+          article_id SERIAL PRIMARY KEY NOT NULL, 
           title VARCHAR NOT NULL, 
           body VARCHAR NOT NULL, 
           votes INT, 
-          topic VARCHAR NOT NULL REFERENCES topic(slug), 
-          author VARCHAR PRIMARY KEY NOT NULL REFERENCES user(username), 
+          topic VARCHAR NOT NULL REFERENCES topics(slug), 
+          author VARCHAR NOT NULL REFERENCES users(username), 
           created_at TIMESTAMP)`
       );
     })
     .then(() => {
       return db.query(
-        `CREATE TABLE comment (
+        `CREATE TABLE comments (
           comment_id SERIAL PRIMARY KEY, 
-          author VARCHAR PRIMARY KEY NOT NULL REFERENCES user(username), 
-          articel_id PRIMARY KEY REFERENCES article(article_id), 
+          author VARCHAR NOT NULL REFERENCES users(username), 
+          article_id INT REFERENCES articles(article_id), 
           votes INT, 
           created_at TIMESTAMP, 
           body VARCHAR NOT NULL)`
@@ -57,38 +57,54 @@ const seed = (data) => {
     })
     .then(() => {
       const querySTR = format(
-        `INSERT INTO topic (slug, description) VALUES %L RETURNING *;`,
+        `INSERT INTO topics (slug, description) VALUES %L RETURNING *;`,
         topicData.map((topic) => {
           return [topic.slug, topic.description];
+        })
+      );
+
+      return db.query(querySTR);
+    })
+    .then(() => {
+      const querySTR = format(
+        `INSERT INTO users (username, avatar_url, name) VALUES %L RETURNING *;`,
+        userData.map((user) => {
+          return [user.username, user.avatar_url, user.name];
         })
       );
       return db.query(querySTR);
     })
     .then(() => {
       const querySTR = format(
-        `INSERT INTO user (username, avatar_url, name) VALUES %L RETURNING *;`,
-        userDATA.map((user) => {
-          return [user.username, user.avatar_url, user.name];
+        `INSERT INTO articles (title, body, votes, topic, author, created_at) VALUES %L RETURNING *;`,
+        articleData.map((article) => {
+          return [
+            article.title,
+            article.body,
+            article.votes || 0,
+            article.topic,
+            article.author,
+            article.created_at,
+          ];
         })
       );
-      //return
-      db.query(querySTR);
+      return db.query(querySTR);
+    })
+    .then(() => {
+      const querySTR = format(
+        `INSERT INTO comments (author, article_id, votes, created_at, body) VALUES %L RETURNING *;`,
+        commentData.map((comment) => {
+          return [
+            comment.author,
+            comment.article_id,
+            comment.votes || 0,
+            comment.created_at,
+            comment.body,
+          ];
+        })
+      );
+      return db.query(querySTR);
     });
-  .then(() => {
-    const formattedArticleData = articleData.map(article) => {
-      let topic = generateTopic(article, topicData);
-      let author = generateAuthor(article, userData);
-      return {
-        "article_id": article.article_id,
-        "title": article.title,
-        "body": article.body,
-        "votes": 0,
-        "topic": topic,
-        "author": author,
-        "created_at": time_stamp
-      }
-    });
-  });
 };
 
 module.exports = seed;
