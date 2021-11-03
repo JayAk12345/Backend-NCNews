999910e7aece73c53868ce047e189c4d56a38736
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-03 13:04:45 +0000

    removes handlebadurl error handling as not needed in routers

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,10 +1,7 @@
 const apiRouter = require("express").Router();
 const topicsRouter = require("./topicsRouter");
-const { handleBadUrls } = require("../controllers/errorController");
 
 console.log("in api router");
 apiRouter.use("/topics", topicsRouter);
-
-apiRouter.use("*", handleBadUrls);
 
 module.exports = apiRouter;
--- routers/topicsRouter.js
+++ routers/topicsRouter.js
@@ -1,10 +1,7 @@
 const topicsRouter = require("express").Router();
 const { getTopics } = require("../controllers/topicsController");
-const { handleBadUrls } = require("../controllers/errorController");
 
 console.log("in topic router");
 topicsRouter.route("/").get(getTopics);
-
-topicsRouter.use("*", handleBadUrls);
 
 module.exports = topicsRouter;
