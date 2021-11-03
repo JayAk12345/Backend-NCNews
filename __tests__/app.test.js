16d68a13c187f4ca401fa4abac244ab079435957
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-03 12:44:34 +0000

    adds completed test for api/topics endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,12 @@ const db = require("../db/connection.js");
 const testData = require("../db/data/test-data/index.js");
 const seed = require("../db/seeds/seed.js");
 const app = require("../app.js");
+const {
+  topicData: topicDataTest,
+  userData: userDataTest,
+  articleData: articleDataTest,
+  commentData: commentDataTest,
+} = testData;
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -22,13 +28,24 @@ describe("/api/wrongpath", () => {
 
 describe.only("/api/topics", () => {
   describe("GET", () => {
-    it("returns an object", () => {
+    // it("returns an object", () => {
+    //   return request(app)
+    //     .get("/api/topics")
+    //     .expect(200)
+    //     .then((res) => {
+    //       console.log(res.body);
+    //     });
+    // });
+    it("returns all topics in the correct structure of an object with a key of what is being returned and a value of an array of topic objects", () => {
+      let correctStructure = { topics: topicDataTest };
       return request(app)
         .get("/api/topics")
         .expect(200)
         .then((res) => {
-          expect(typeof res.body.topics).toBe("object");
-          expect(res.body.topics.length).toBe(undefined);
+          expect(typeof res.body).toBe("object");
+          expect(res.body.length).toBe(undefined);
+          console.log(res, "test");
+          expect(res.body).toEqual(correctStructure);
         });
     });
   });
