d907e9b546d2e1fdfcadcee458e98bf78bc09593
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-03 13:04:10 +0000

    adds error handling tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -26,17 +26,9 @@ describe("/api/wrongpath", () => {
   });
 });
 
-describe.only("/api/topics", () => {
+describe("/api/topics", () => {
   describe("GET", () => {
-    // it("returns an object", () => {
-    //   return request(app)
-    //     .get("/api/topics")
-    //     .expect(200)
-    //     .then((res) => {
-    //       console.log(res.body);
-    //     });
-    // });
-    it("returns all topics in the correct structure of an object with a key of what is being returned and a value of an array of topic objects", () => {
+    it("status(200), returns all topics in the correct structure of an object with a key of what is being returned and a value of an array of topic objects", () => {
       let correctStructure = { topics: topicDataTest };
       return request(app)
         .get("/api/topics")
@@ -44,9 +36,18 @@ describe.only("/api/topics", () => {
         .then((res) => {
           expect(typeof res.body).toBe("object");
           expect(res.body.length).toBe(undefined);
-          console.log(res, "test");
           expect(res.body).toEqual(correctStructure);
+        });
+    });
+    it("status(404) returns error message of Not found", () => {
+      return request(app)
+        .get("/api/topi")
+        .expect(404)
+        .then((res) => {
+          expect(res.body.msg).toBe("Not found");
         });
     });
   });
 });
+
+//describe("/api/articles/:article_id", () => {});
