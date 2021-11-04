89a57231378c43e28faf973fe2ad20a28550f5d6
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-04 12:51:50 +0000

    adds catch

--- controllers/topicsController.js
+++ controllers/topicsController.js
@@ -1,8 +1,12 @@
 const { fetchTopics } = require("../models/topicsModel");
 
-exports.getTopics = (req, res) => {
-  fetchTopics().then((response) => {
-    let returnObj = { topics: response };
-    res.status(200).send(returnObj);
-  });
+exports.getTopics = (req, res, next) => {
+  fetchTopics()
+    .then((response) => {
+      let returnObj = { topics: response };
+      res.status(200).send(returnObj);
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
