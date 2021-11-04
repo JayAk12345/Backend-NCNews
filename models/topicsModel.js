af3008641d069f33144c5bf2e018c9995ae88313
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-04 12:58:17 +0000

    removes catch

--- models/topicsModel.js
+++ models/topicsModel.js
@@ -1,12 +1,7 @@
 const db = require("../db/connection");
 
 exports.fetchTopics = () => {
-  return db
-    .query(`SELECT * FROM topics`)
-    .then((res) => {
-      return res.rows;
-    })
-    .catch((err) => {
-      return Promise.reject({ status: 404, msg: "Not found" });
-    });
+  return db.query(`SELECT * FROM topics`).then((res) => {
+    return res.rows;
+  });
 };
