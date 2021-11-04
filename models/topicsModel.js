5db09ac41d51fca947298393e10de9e93a7ca0d3
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-04 12:32:16 +0000

    adds catch

--- models/topicsModel.js
+++ models/topicsModel.js
@@ -1,7 +1,12 @@
 const db = require("../db/connection");
 
 exports.fetchTopics = () => {
-  return db.query(`SELECT * FROM topics`).then((res) => {
-    return res.rows;
-  });
+  return db
+    .query(`SELECT * FROM topics`)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      return Promise.reject({ status: 404, msg: "Not found" });
+    });
 };
