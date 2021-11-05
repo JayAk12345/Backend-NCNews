ed85c9fb56199ac58052877dc3379b0d5e2a6a2e
JayAk12345 <jahanzaib50@hotmail.co.uk>
2021-11-05 16:42:02 +0000

    adds 404 error middleware and in progress controller for articles/:articles_id patch endpoint

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,9 +1,13 @@
-const { fetchArticleById } = require("../models/articleModel");
+const {
+  fetchArticleById,
+  patchArticleVote,
+} = require("../models/articleModel");
 
 exports.getArticleById = (req, res, next) => {
   console.log("in controller");
 
   const { article_id: id } = req.params;
+  console.log(typeof id, "OLD CHECK");
 
   fetchArticleById(id)
     .then((response) => {
@@ -14,6 +18,22 @@ exports.getArticleById = (req, res, next) => {
       }
     })
     .catch((err) => {
+      next(err);
+    });
+};
+
+exports.updateArticleVote = (req, res, next) => {
+  console.log("in controller");
+  const { inc_votes } = req.body;
+  const { article_id: id } = req.params;
+  console.log(typeof inc_votes, typeof id, "TYPES");
+
+  patchArticleVote(inc_votes, id)
+    .then((response) => {
+      res.status(201).send(response);
+    })
+    .catch((err) => {
+      console.log(err, "catch error");
       next(err);
     });
 };
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,11 +4,18 @@ exports.handleBadUrls = (req, res) => {
 };
 
 exports.handle400Errors = (err, req, res, next) => {
+  if (err.status !== 400) {
+    next(err);
+  }
   console.log("HITTING 400 ERROR");
   res.status(400).send({ msg: "Bad request" });
 };
+exports.handle404Errors = (err, req, res, next) => {
+  console.log("HITTING 404 ERROR");
+  res.status(404).send({ msg: "Not found" });
+};
 
-exports.handle500Errors = (err, req, res, next) => {
+exports.handle500Errors = (err, req, res) => {
   console.log(err.code);
   res.status(500).send("500 error");
 };
