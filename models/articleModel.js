b5feaf109c62a03b2487e71f2a936e7e59330f2a
JayAk12345 <jahanzaib50@hotmail.co.uk>
2022-02-06 18:02:54 +0000

    completes patchArticleVote

--- models/articleModel.js
+++ models/articleModel.js
@@ -22,14 +22,19 @@ exports.patchArticleVote = (inc_votes, id) => {
   WHERE articles.article_id = $1
   RETURNING *;`;
   const queryParams = [id, inc_votes];
+  console.log("IN MODEL");
 
   return db.query(queryStr, queryParams).then((res) => {
+    console.log("IN RETURN OF MODEL");
     if (res.rows[0] === undefined) {
+      console.log("IN MODEL RETURN IF");
       return Promise.reject({
         status: 404,
         msg: "Not found",
       });
+    } else {
+      console.log(res.rows[0], "MODEL RETURN ELSE");
+      return res.rows[0];
     }
-    return res.rows[0];
   });
 };
