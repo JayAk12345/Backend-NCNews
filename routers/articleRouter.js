1033197e6e9065647073867b23485b81d6e7c51e
JayAk12345 <jahanzaib50@hotmail.co.uk>
2022-02-06 18:09:52 +0000

    adds middleware for the api/articles endpoint

--- routers/articleRouter.js
+++ routers/articleRouter.js
@@ -2,9 +2,11 @@ const articleRouter = require("express").Router();
 const {
   getArticleById,
   updateArticleVote,
+  getArticles,
 } = require("../controllers/articleController");
 
 articleRouter.route("/:article_id").get(getArticleById);
 articleRouter.route("/:article_id").patch(updateArticleVote);
+articleRouter.route("/").get(getArticles);
 
 module.exports = articleRouter;
